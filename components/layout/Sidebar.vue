ed8f52cdb0d90d8ccaf436ba78235d9e385f67c0
Pavel1Uvarov <pavel2uvarov@gmail.com>
2024-06-05 20:01:26 +0300

    feat(Logo): add logo

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,5 +1,11 @@
 <template>
 	<aside class="bg-gray-900 p-4 h-full relative min-h-[100vh]">
+		<div class="flex justify-center mb-3">
+			<NuxtImg
+				src="/assets/images/logo.svg"
+				width="140"
+			/>
+		</div>
 		<LayoutMenu />
 	</aside>
 </template>
