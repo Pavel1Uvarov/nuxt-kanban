44a94707a4330fe6deebc255e04c5974ffd20d00
Pavel1Uvarov <pavel2uvarov@gmail.com>
2024-06-06 22:33:44 +0300

    feat(HomePage): add drag drop logic

--- composables/useKanbanQuery.ts
+++ composables/useKanbanQuery.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/vue-query';
 import { KANBAN_DATA } from '~/data/kanban.data';
+import type { IColumn } from '~/types/kanban/kanban.types';
 import type { IRecord } from '~/types/records.types';
 
 export const useKanbanQuery = () => {
@@ -10,14 +11,17 @@ export const useKanbanQuery = () => {
 		queryKey: ['records'],
 		queryFn: async () => appwrite.DB.listDocuments(config.public.APP_WRITE_PROJECT_DB_ID, config.public.APP_WRITE_RECORDS_COLLECTION_ID),
 		select(data) {
-			const newBoard = [...KANBAN_DATA];
+			const newBoard: IColumn[] = KANBAN_DATA.map(column => ({
+				...column,
+				cards: [],
+			}));
 			const records = data.documents as unknown as IRecord[];
 
 			records.forEach((record) => {
 				const column = newBoard.find(column => column.id === record.status);
 				if (column) {
 					column.cards.push({
-						id: record.id,
+						id: record.$id,
 						name: record.name,
 						$createdAt: record.$createdAt,
 						status: record.status,
--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,10 @@
 
 		<div class="grid grid-cols-4 gap-16 mt-4">
 			<div
-				v-for="(column, index) in data"
-				:key="index"
+				v-for="(column) in data"
+				:key="column.id"
+				@dragover="handleDragOver"
+				@drop="handleDragEnd(column)"
 			>
 				<div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center text-white">
 					{{ column.name }}
@@ -16,6 +18,8 @@
 					<UiCard
 						v-for="(card, index) in column.cards"
 						:key="index"
+						draggable="true"
+						@dragstart="handleDragStart(card, column)"
 					>
 						<UiCardHeader>
 							<UiCardTitle>Card Title</UiCardTitle>
@@ -51,31 +55,72 @@
 </template>
 
 <script setup lang="ts">
+import { useMutation } from '@tanstack/vue-query';
 import { useKanbanQuery } from '@/composables/useKanbanQuery';
-import type { EnumStatus } from '~/types/records.types';
+import type { ICard, IColumn } from '~/types/kanban/kanban.types';
+import type { EnumStatus, IRecord } from '~/types/records.types';
 
 useSeoMeta({
 	title: 'Home | CRM Kanban',
 });
 
+const appwrite = useAppWrite();
+const config = useRuntimeConfig();
+
 const selectedColumn = ref('todo');
+const dragCard = ref({});
+const sourceColumn = ref({});
 const ui = reactive({
 	showCardFromModal: false,
 });
 
 const { data, refetch } = useKanbanQuery();
 
+type TypeMutationVariables = {
+	docId: string;
+	status: EnumStatus;
+};
+
+const { mutate } = useMutation({
+	mutationKey: ['move card'],
+	mutationFn: async ({ docId, status }: TypeMutationVariables) => {
+		return await appwrite.DB.updateDocument(
+			config.public.APP_WRITE_PROJECT_DB_ID,
+			config.public.APP_WRITE_RECORDS_COLLECTION_ID,
+			docId, { status });
+	},
+	onSuccess: () => {
+		refetch();
+	},
+});
+
 const handleCardFormModal = (columnId: EnumStatus) => {
 	selectedColumn.value = columnId;
 	ui.showCardFromModal = true;
 };
 
-const closeCardFormModal = () => {
-	ui.showCardFromModal = false;
-};
+const closeCardFormModal = () => ui.showCardFromModal = false;
 
 const submittedCard = () => {
 	ui.showCardFromModal = false;
 	refetch();
+};
+
+const handleDragStart = (record: ICard, column: IColumn) => {
+	dragCard.value = record;
+	sourceColumn.value = column;
+};
+
+const handleDragOver = (event: DragEvent) => {
+	event.preventDefault();
+};
+
+const handleDragEnd = (targetColumn: IColumn) => {
+	if (dragCard.value.id && sourceColumn.value.id) {
+		mutate({ docId: dragCard.value.id, status: targetColumn.id });
+	}
+
+	dragCard.value = {};
+	sourceColumn.value = {};
 };
 </script>
